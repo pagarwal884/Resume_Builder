a3f5530c9278b552b0fc83c0a0af493a92222012
pagarwal884 <pagarwal1145@gmail.com>
2025-07-17 06:06:16 +0530

    Done few work in Resumecontroller file in which i have added function to get resume by id and by User Id too

--- backend/controllers/resumeController.js
+++ backend/controllers/resumeController.js
@@ -67,5 +67,52 @@ export const createResume = async (req, res) => {
       ],
       interests: [""],
     };
-  } catch (error) {}
+
+    const newResume = await Resume.create({
+      UserID: req.User._id,
+      title,
+      ...defaultResumeData,
+      ...req.body,
+    });
+
+    res.status(201).json(newResume);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Failed To create Resume", error: error.message });
+  }
+};
+
+// Get Function
+export const getUserResume = async (req, res) => {
+  try {
+    const resumes = await Resume.find({ UserID: req.User._id }).sort({
+      updatedAt: -1,
+    });
+    res.json(resumes);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Failed to fetch resumes", error: error.message });
+  }
+};
+
+// ================= GET RESUME BY ID =================
+export const getResumebyID = async (req, res) => {
+  try {
+    const resume = await Resume.findOne({
+      _id: req.params.id,
+      UserID: req.User._id,
+    });
+
+    if (!resume) {
+      return res.status(404).json({ message: "Resume not found" });
+    }
+
+    res.status(200).json(resume);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Failed to fetch resume", error: error.message });
+  }
 };
