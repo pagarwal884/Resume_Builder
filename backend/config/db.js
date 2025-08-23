e43f8ef9abd53ba6425e03a193f4b929edb099af
pagarwal884 <pagarwal1145@gmail.com>
2025-08-23 08:05:35 +0530

    Resolve a error related to database which has been occured suddenly

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   await mongoose
     .connect(
-      "mongodb+srv://pagarwal1145:pagarwal1145@cluster0.fyxhv4v.mongodb.net/RESUME"
+      "mongodb+srv://pagarwal1145:pagarwal1145@cluster0.fyxhv4v.mongodb.net/"
     )
     .then(() => console.log("DB CONNECTED"));
 };
